24ccd4e568df3e98a2722a290a2e9a5379209644
Mark Benner <goldfisho15@gmail.com>
2016-11-09 16:56:38 +1300

    Added sidebar

--- Gemfile
+++ Gemfile
@@ -2,4 +2,7 @@
 source "https://rubygems.org"
 gem "jekyll-coffeescript"
 gem "jekyll"
+group :jekyll_plugins do
+    gem 'jekyll-livereload'
+end
 # gem "rails"
--- _plugins/breadcrumbs_plugin.rb
+++ _plugins/breadcrumbs_plugin.rb
@@ -7,6 +7,13 @@ module Jekyll
       url.shift
       url.map.each_with_index { |segment, i| "<a href='#{ '/' + url[0,i+1].join('/') }'>#{segment}</a>" }.join(" / ")
     end
+
+    def urlmatch_plugin(input)
+      return '' if input.nil?
+      url = input.split('/')
+      url.pop
+      url.join('/') + '/'
+    end
   end
 end
 
