35a49099781539956e68c4531b8018bc24bcb4b7
Daniel Brooker <dan@nocturnalcode.com>
2017-02-11 22:32:39 +1300

    homepage js

--- Gemfile
+++ Gemfile
@@ -1,8 +1,4 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
-gem "jekyll-coffeescript"
 gem "jekyll"
-group :jekyll_plugins do
-    gem 'jekyll-livereload'
-end
-# gem "rails"
+
--- js/application.js
+++ js/application.js
@@ -1,23 +1,70 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// the compiled file.
-//
-// WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
-// GO AFTER THE REQUIRES BELOW.
-//
-//= require jquery
-//= require jquery_ujs
-//= require_tree .
+$(function() {
 
-$(".fade").each(function(evt){
-	$(this).load(function(evt){
-  		$(this).fadeIn(1000);
-  		$(this).show();
-	});
+  $(".fade").each(function(evt){
+    $(this).load(function(evt){
+        $(this).fadeIn(1000);
+        $(this).show();
+    });
+  });
+
+  if ($('video').is('*')) {
+      $('body').append('<link href="http://vjs.zencdn.net/c/video-js.css" rel="stylesheet">')
+      $('body').append('<script src="http://vjs.zencdn.net/c/video.js"></script>')
+  }
+
+
+  let selectTab = function(content){
+    $("#universal-access").hide();
+    $("#improved-performance").hide();
+    $("#improved-profitability").hide();
+    $("#unique-functionality").hide();
+    return $(content).show();
+  };
+
+  let nextTab = function() {
+    if ($("#hero").is(":hover")) {
+      return;
+    }
+    if (window.youtube_playing) {
+      return;
+    }
+
+    $(".fade","#universal-access").show();
+    if ($("#universal-access").is(":visible")) {
+      return selectTab("#improved-performance");
+    } else if ($("#improved-performance").is(":visible")) {
+      return selectTab("#improved-profitability");
+    } else if ($("#improved-profitability").is(":visible")) {
+      return selectTab("#unique-functionality");
+    } else if ($("#unique-functionality").is(":visible")) {
+      return selectTab("#universal-access");
+    }
+  };
+
+  let selectContact = function(content,button){
+    $("#new-zealand").hide();
+    $("#melbourne").hide();
+    $("#sydney").hide();
+    $("#usa").hide();
+    $(content).show();
+
+    $("#new-zealand-contact-details").removeClass("selected");
+    $("#melbourne-contact-details").removeClass("selected");
+    $("#sydney-contact-details").removeClass("selected");
+    $("#usa-contact-details").removeClass("selected");
+    return $(button).addClass("selected");
+  };
+
+  $(function() {
+    $(".universal-access-button").click(() => selectTab("#universal-access"));
+    $(".improved-performance-button").click(() => selectTab("#improved-performance"));
+    $(".improved-profitability-button").click(() => selectTab("#improved-profitability"));
+    $(".unique-functionality-button").click(() => selectTab("#unique-functionality"));
+    $("#new-zealand-contact-details").click(() => selectContact("#new-zealand","#new-zealand-contact-details"));
+    $("#melbourne-contact-details").click(() => selectContact("#melbourne","#melbourne-contact-details"));
+    $("#usa-contact-details").click(() => selectContact("#usa","#usa-contact-details"));
+    return $("#sydney-contact-details").click(() => selectContact("#sydney","#sydney-contact-details"));
+  });
+  setInterval( ( () => nextTab()) , 4500);
+
 });
-	
